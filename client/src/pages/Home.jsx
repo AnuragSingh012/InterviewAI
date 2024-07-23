dde7befb129efda024a912c9f5d0b347511f4394
Anurag Singh <134362386+AnuragSingh012@users.noreply.github.com>
2024-07-23 19:56:31 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,11 +5,11 @@ const Home = () => {
   return (
     <div className="mx-8">
       <div className="max-w-5xl m-auto mt-28 text-center">
-        <h1 className="max-w-4xl font-inter m-auto text-7xl font-bold text-center">
+        <h1 className="max-w-4xl font-inter m-auto text-5xl sm:text-6xl md:text-7xl font-bold text-center">
           Prepare Like a Pro with <br className="max-md:hidden" />{" "}
           <span className="text-[#fa900a]">AI Interviews</span>
         </h1>
-        <p className="mt-8 max-w-2xl m-auto text-md text-slate-600 text-center">
+        <p className="mt-8 max-w-2xl m-auto text-base md:text-md text-slate-600 text-center">
           Welcome to your ultimate interview preparation platform! Take
           AI-driven mock interviews tailored to your field, practice with
           targeted questions, and receive expert-enhanced answers to boost your
